88436fb725a926bb5f1c08b1a42535fccd14cfed
monish <gsmkumar2002@gmail.com>
2022-05-05 12:54:26 +0530

    new file

--- Day2/day2_bank.cpp
+++ Day2/day2_bank.cpp
@@ -1,37 +1,48 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
-
+class compare; //forward declaration
 //Base classes bank,loan
 class bank{public:
     string bankName,establishedDate,bankType;
     void getName(){
         cout<<"Enter the name of the bank: ";
-        cin>>bankName;
+        getline(cin,bankName);cout<<endl;
     }
     void getEstablishedDate(){
         cout<<"Enter the established date of the bank: ";
-        cin>>establishedDate; cout<<endl;
+        getline(cin,establishedDate); cout<<endl;
     }
     void getBankType(){
         cout<<"Enter the type of the bank: ";
-        cin>>bankType; cout<<endl;
+        getline(cin,bankType); cout<<endl;
     }
     void getInfo(){
-        cout<<"Bank Name: "<<bankName<<endl;
+        cout<<endl<<"Bank Name: "<<bankName<<endl;
         cout<<"Established Date: "<<establishedDate<<endl;
-        cout<<"Bank Type: "<<bankType<<endl;
+        cout<<"Bank Type: "<<bankType<<endl<<endl;
     }
 };
 
 class loan{public:
-    string loanType,loanTenure;
+    string loanType,docsrequired;
     float interestRate;
+    // friend class compare; //friend class
     void getLoanType(){
-        cout<<"Enter the types of the loan available: ";
+        cout<<"Enter the Types of the loan available: ";
         getline(cin, loanType);
     }
+    void getDocsRequired(){
+        cout<<"Enter the docs required for the loan: ";
+        getline(cin, docsrequired);
+    }
+    void getInterestRate(){
+        cout<<"Enter the interest rate of the loan: ";
+        cin>>interestRate;
+    }
 };
+
 //---------------------------------------------------------------------------------------------------------------------
 class HDFC:public bank,public loan{public:
     void getName(){
@@ -95,3 +106,22 @@ class IOB:public bank,public loan{public:
     }
 };
 //-----------------------------------------------------------------------------------------------------------------------------
+
+class compare{public:
+
+    //compare interest Rates of different banks
+    void compareInterestRate(HDFC h,SBI s,ICICI i,IOB o){
+        if(h.interestRate>s.interestRate&&h.interestRate>i.interestRate&&h.interestRate>o.interestRate){
+            cout<<"HDFC has the highest interest rate"<<endl;
+        }
+        else if(s.interestRate>h.interestRate&&s.interestRate>i.interestRate&&s.interestRate>o.interestRate){
+            cout<<"SBI has the highest interest rate"<<endl;
+        }
+        else if(i.interestRate>h.interestRate&&i.interestRate>s.interestRate&&i.interestRate>o.interestRate){
+            cout<<"ICICI has the highest interest rate"<<endl;
+        }
+        else if(o.interestRate>h.interestRate&&o.interestRate>s.interestRate&&o.interestRate>i.interestRate){
+            cout<<"IOB has the highest interest rate"<<endl;
+        }
+    }
+};
