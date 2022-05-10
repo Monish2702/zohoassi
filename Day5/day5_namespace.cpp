405552e7ee3e4c19a816d491ca236a1764f336b7
monish <gsmkumar2002@gmail.com>
2022-05-10 12:40:00 +0530

    day5

--- Day5/day5_namespace.cpp
+++ Day5/day5_namespace.cpp
@@ -6,22 +6,21 @@ using namespace std;
 class class_to_import_namespace { public:
 
     void root_display() {
+        namespace p=parent_namespace; //namespace aliasing or using namespace Test;
+        p::child_namespace::quad q;
 
-    namespace p=parent_namespace; //namespace aliasing or using namespace Test;
-    p::child_namespace::quad q;
-
-    roots var;
-    var = q.roots_of_quadratic(1, -6, 5); // x^2 - 6x + 5 
-    if(var.x == true){
-        cout << "Roots are real and they are :" << endl;
-        cout << "x1 = " << var.x1 << endl;
-        cout << "x2 = " << var.x2 << endl;
-    }
-    else{
-        cout << "Roots are imaginary" << endl;
-    }
-
-    cout << p::Func() << endl; //Hello from new
+        roots var;
+        var = q.roots_of_quadratic(1, -6, 5); // x^2 - 6x + 5 
+        if(var.x == true) {
+            cout << "Roots are real and they are :" << endl;
+            cout << "x1 = " << var.x1 << endl;
+            cout << "x2 = " << var.x2 << endl;
+        }
+        else {
+            cout << "Roots are imaginary" << endl;
+        }
+    
+        cout << p::Func() << endl; //Hello from new
     }
 };
 
