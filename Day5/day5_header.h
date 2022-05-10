9fb1a325dc7ac47c4381d890a6ed7b0dc52e7cad
monish <gsmkumar2002@gmail.com>
2022-05-10 12:11:31 +0530

    day5

--- Day5/day5_header.h
+++ Day5/day5_header.h
@@ -1,26 +1,33 @@
 //day5_header.h
 #include <string>
 #include <cmath>
+struct roots{
+    double x1,x2;
+    bool x;
+};
 
 namespace parent_namespace //parent namespace
 {
     namespace child_namespace //normal namespace
     {
        class quad {public: //class to find roots of quadratic equations
-        double roots_of_quadratic(double a, double b, double c){  //function
+       roots r;
+        roots roots_of_quadratic(double a, double b, double c){  //function
             double x1, x2;
             double d = b*b - 4*a*c;
             if(d < 0){
-                return -1;
+                r.x = false;
+                return r;
             }
             else if(d == 0){
-                x1 = -b/(2*a);
-                return x1;
+                r.x1 = -b/(2*a);
+                r.x2 = -b/(2*a);
+                return r;
             }
             else{
-                x1 = (-b + sqrt(d))/(2*a);
-                x2 = (-b - sqrt(d))/(2*a);
-                return x1;
+                r.x1 = (-b + sqrt(d))/(2*a);
+                r.x2 = (-b - sqrt(d))/(2*a);
+                return r;
             }
         }
      };
@@ -28,6 +35,6 @@ namespace parent_namespace //parent namespace
 
     inline namespace print_inline //inline namespace (here no class only function)
     {
-        std::string Func() { return std::string("Hello from inline namespace"); }
+        std::string Func() { return std::string("\nHello from inline namespace"); }
     }
 }
--- Day5/day5_namespace.cpp
+++ Day5/day5_namespace.cpp
@@ -8,10 +8,18 @@ class class_to_import_namespace { public:
     void root_display() {
 
     namespace p=parent_namespace; //namespace aliasing or using namespace Test;
-    p::child_namespace::quad r;
+    p::child_namespace::quad q;
 
-    double root = r.roots_of_quadratic(1, -6, 5); // x^2 - 6x + 5 
-    cout <<"The root of the given x^2 - 6x + 5 equation is: "<< root << endl;
+    roots var;
+    var = q.roots_of_quadratic(1, -6, 5); // x^2 - 6x + 5 
+    if(var.x == true){
+        cout << "Roots are real and they are :" << endl;
+        cout << "x1 = " << var.x1 << endl;
+        cout << "x2 = " << var.x2 << endl;
+    }
+    else{
+        cout << "Roots are imaginary" << endl;
+    }
 
     cout << p::Func() << endl; //Hello from new
     }
