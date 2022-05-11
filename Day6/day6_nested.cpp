e74ae2e8614b110795225aa7e602fc22ea83c27a
monish <gsmkumar2002@gmail.com>
2022-05-11 14:30:57 +0530

    day6

--- Day6/day6_nested.cpp
+++ Day6/day6_nested.cpp
@@ -3,21 +3,27 @@
 #include <exception>
 
 int main(){
+    /*Outer try block*/
     try{
+        throw std::runtime_error("\nRuntime error outer try block");
+        /*Inner try block*/
         try{
-            throw std::runtime_error("Invalid argument");
-        }
+            throw std::runtime_error("Runtime error inner try block");
+            throw 20;
+        }  /*Inner catch block*/
         catch(std::runtime_error &e){
             std::cout << e.what() << std::endl;
+            std::cout<<"Rethrowing an already catched exception\n"<<std::endl;
             throw;
         }
     }
+    /*Outer catch block-1*/
     catch(std::invalid_argument &e){
         std::cout << "Invalid argument: " << e.what() << std::endl;
-    }
-    // catch(std::runtime_error &e){
-    //     std::cout << e.what() << std::endl;
-    // }
+    } /*Outer catch block-2*/
+    catch(std::runtime_error &e){
+        std::cout << e.what() << std::endl;
+    } /*Outer catch block-3*/
     catch(...){
         std::cout << "Unknown error" << std::endl;
     }
