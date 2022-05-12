c68b783d46e2df7b2503704def21b3a4f02a24c0
monish <gsmkumar2002@gmail.com>
2022-05-12 22:25:42 +0530

    day1 edited

--- Day1/day1_vehicle.cpp
+++ Day1/day1_vehicle.cpp
@@ -1,24 +1,40 @@
 #include <iostream>
 #include <string>
 using namespace std;
-enum vehicle_type
+enum vehicleType
 {
     petrol,
     diesel,
     electric,
     hybrid
 };
+const char *vehicleTypeString[] = {"petrol", "diesel", "electric", "hybrid"};
+
+enum carType
+{
+    sedan,
+    suv,
+    hatchback
+};
+const char *carTypeString[] = {"sedan", "suv", "hatchback"};
+
+enum bikeType
+{
+    scooter,
+    gear_bike
+};
+const char *bicycleTypeString[] = {"scooter", "gear_bike"};
 
 // Base class vehicle
 class Vehicle
 {
 public:
-    vehicle_type type;
+    vehicleType type;
     string brand, model, color;
     int no_of_wheels;
     float mileage, price;
     // Base class constructor
-    Vehicle(vehicle_type type, string brand, string model, string color, float mileage, float price)
+    Vehicle(vehicleType type, string brand, string model, string color, float mileage, float price)
     {
         this->type = type;
         this->brand = brand;
@@ -33,15 +49,17 @@ public:
     //     return this->price < v1.price;
     // }
     friend ostream &operator<<(ostream &print, const Vehicle &v);
-    virtual void get_no_of_wheels() = 0;
+    virtual int getNoOfWheels() = 0;
 };
+
 bool operator<(const Vehicle &v1, const Vehicle &v2)
-{ // friend funcction to comapre prices
+{ // friend function to comapre prices
     return v1.price < v2.price;
 }
+
 ostream &operator<<(ostream &print, const Vehicle &v)
 { // friend function to print vehicle details
-    print << "Vehicle type: " << v.type << endl;
+    print << "Vehicle type: " << vehicleTypeString[v.type] << endl;
     print << "Brand: " << v.brand << endl;
     print << "Model: " << v.model << endl;
     print << "Color: " << v.color << endl;
@@ -55,18 +73,16 @@ class Car : public Vehicle
 {
 public:
     int no_of_persons;
-    string car_type;
+    carType car_type;
     // Derived class-1 constructor
-    Car(vehicle_type type, string brand, string model, string color, float mileage, float price, int no_of_persons, string car_type) : Vehicle(type, brand, model, color, mileage, price)
+    Car(vehicleType type, string brand, string model, string color, float mileage, float price, int no_of_persons, carType car_type) : Vehicle(type, brand, model, color, mileage, price)
     {
         this->no_of_persons = no_of_persons;
         this->car_type = car_type;
     }
-    void get_no_of_wheels()
+    int getNoOfWheels()
     {
-        cout << "Enter no of wheels in your car: " << endl;
-        cin >> no_of_wheels;
-        cout << "No of wheels in your car: " << no_of_wheels << endl;
+        return 4;
     }
 };
 
@@ -76,44 +92,68 @@ class Bike : public Vehicle
 {
 public:
     float weight;
-    string bike_type;
+    // vehicleType type;
+    bikeType bike_type;
     // Derived class-2 constructor
-    Bike(vehicle_type type, string brand, string model, string color, float mileage, float price, float weight, string bike_type) : Vehicle(type, brand, model, color, mileage, price)
+    Bike(vehicleType type, string brand, string model, string color, float mileage, float price, float weight, bikeType bike_type) : Vehicle(type, brand, model, color, mileage, price)
     {
         this->weight = weight;
         this->bike_type = bike_type;
     }
-    void get_no_of_wheels()
+    int getNoOfWheels()
     {
-        cout << "Enter no of wheels in your bike: " << endl;
-        cin >> no_of_wheels;
-        cout << "No of wheels in your bike: " << no_of_wheels << endl;
+        return 2;
+        // cout << "Enter no of wheels in your bike: " << endl;
+        // cin >> no_of_wheels;
+        // cout << "No of wheels in your bike: " << no_of_wheels << endl;
     }
 };
 
 //---------------------------------------------------------------------------------------------------------------------------------
 int main()
 {
-    cout << "\nVehicle_type: 0-petrol 1-diesel 2-electric 3-hybrid\n";
-    Car c1(hybrid, "honda", "civic", "red", 25, 12000.8f, 4, "sedan");
-    cout << "For car 1: ";
-    c1.get_no_of_wheels();
-    Car c2(diesel, "honda", "city", "black", 23.4, 15000.5f, 4, "sedan");
-    cout << "For car 2: ";
-    c2.get_no_of_wheels();
-    int c = c1 < c2;
-    cout << "Is c1<c2?: " << c << endl;
-    Bike b(petrol, "hero honda", "splender", "blue", 50.2, 12000.4f, 100, "mountain");
-    cout << "For bike: ";
-    b.get_no_of_wheels();
+    Car c1(hybrid, "honda", "civic", "red", 25, 12000.8f, 4, sedan);
+    Car c2(diesel, "honda", "city", "black", 23.4, 15000.5f, 4, suv);
+    bool c = c1 < c2;
+
+    // cout << "Is c1<c2?: " << c << endl;
+    Bike b(petrol, "hero honda", "splender", "blue", 50.2, 12000.4f, 100, scooter);
     cout << endl
          << "------------------------------------------" << endl;
+    //-----------------------------------------------------------------------------------------
+    cout << "For car 1:\n"
+         << endl;
     cout << c1 << endl;
+    cout << "Car Type: " << carTypeString[c1.car_type] << endl;
+    cout << "Number of wheels in car 1: " << c1.getNoOfWheels() << endl;
     cout << endl
          << "------------------------------------------" << endl;
+    //-----------------------------------------------------------------------------------------
+    cout << "For car 2:\n"
+         << endl;
     cout << c2 << endl;
+    cout << "Car Type: " << carTypeString[c2.car_type] << endl;
+    cout << "Number of wheels in car 2: " << c2.getNoOfWheels();
     cout << endl
          << "------------------------------------------" << endl;
+    //-----------------------------------------------------------------------------------------
+    cout << "For bike:\n"
+         << endl;
     cout << b << endl;
+    cout << "Bike Type: " << bicycleTypeString[b.bike_type] << endl;
+    cout << "Number of wheels in bike: " << b.getNoOfWheels() << endl;
+    cout << endl
+         << "------------------------------------------" << endl;
+    //-----------------------------------------------------------------------------------------
+    if (c == 1)
+    {
+        cout << "Car 1 is cheaper than car 2" << endl;
+    }
+    else
+    {
+        cout << "Car 2 is cheaper than car 1" << endl;
+    }
+    cout << endl
+         << "------------------------------------------" << endl;
     return 0;
 }
